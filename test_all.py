ec746bfdf14cda015fd4e2c3aea1555412d59aaa
koo-lab <koolaboratory.gmail.com>
2021-06-28 09:53:05 -0400

    updates

--- test_all.py
+++ test_all.py
@@ -42,54 +42,54 @@ with open(os.path.join(results_path, 'results.txt'), 'w') as fout:
     for model_name in model_names:
         for activation in activations:
 
-        # build model
-        if model_name == 'deepsea':
-            model = model_zoo.deepsea((L,A), num_labels, activation)
-        elif model_name == 'danq':
-            model = model_zoo.danq((L,A), num_labels, activation)
-        elif model_name == 'basset':
-            model = model_zoo.basset((L,A), num_labels, activation)
-        elif model_name == 'deepatt':
-            model = model_zoo.deepatt((L,A), num_labels, activation)
-        elif model_name == 'cnn_att':
-            model = model_zoo.cnn_att((L,A), num_labels, activation)
-        elif model_name == 'cnn_lstm_trans_1':
-            model = model_zoo.cnn_lstm_trans((L,A), num_labels, activation, num_layers=1)
-        elif model_name == 'cnn_lstm_trans_2':
-            model = model_zoo.cnn_lstm_trans((L,A), num_labels, activation, num_layers=2)
-        elif model_name == 'cnn_lstm_trans_4':
-            model = model_zoo.cnn_lstm_trans((L,A), num_labels, activation, num_layers=4)
-        elif model_name == 'cnn_trans_1':
-            model = model_zoo.cnn_trans((L,A), num_labels, activation, num_layers=1)
-        elif model_name == 'cnn_trans_2':
-            model = model_zoo.cnn_trans((L,A), num_labels, activation, num_layers=2)
-        elif model_name == 'cnn_trans_4':
-            model = model_zoo.cnn_trans((L,A), num_labels, activation, num_layers=4)
-        elif model_name == 'deepsea_custom':
-            model = model_zoo.deepsea_custom((L,A), num_labels, activation)
-        elif model_name == 'danq_custom':
-            model = model_zoo.danq_custom((L,A), num_labels, activation)
-        elif model_name == 'basset_custom':
-            model = model_zoo.basset_custom((L,A), num_labels, activation)
-        else:
-            print("can't find model")
-        print(model_name + '_' + activation)
+            # build model
+            if model_name == 'deepsea':
+                model = model_zoo.deepsea((L,A), num_labels, activation)
+            elif model_name == 'danq':
+                model = model_zoo.danq((L,A), num_labels, activation)
+            elif model_name == 'basset':
+                model = model_zoo.basset((L,A), num_labels, activation)
+            elif model_name == 'deepatt':
+                model = model_zoo.deepatt((L,A), num_labels, activation)
+            elif model_name == 'cnn_att':
+                model = model_zoo.cnn_att((L,A), num_labels, activation)
+            elif model_name == 'cnn_lstm_trans_1':
+                model = model_zoo.cnn_lstm_trans((L,A), num_labels, activation, num_layers=1)
+            elif model_name == 'cnn_lstm_trans_2':
+                model = model_zoo.cnn_lstm_trans((L,A), num_labels, activation, num_layers=2)
+            elif model_name == 'cnn_lstm_trans_4':
+                model = model_zoo.cnn_lstm_trans((L,A), num_labels, activation, num_layers=4)
+            elif model_name == 'cnn_trans_1':
+                model = model_zoo.cnn_trans((L,A), num_labels, activation, num_layers=1)
+            elif model_name == 'cnn_trans_2':
+                model = model_zoo.cnn_trans((L,A), num_labels, activation, num_layers=2)
+            elif model_name == 'cnn_trans_4':
+                model = model_zoo.cnn_trans((L,A), num_labels, activation, num_layers=4)
+            elif model_name == 'deepsea_custom':
+                model = model_zoo.deepsea_custom((L,A), num_labels, activation)
+            elif model_name == 'danq_custom':
+                model = model_zoo.danq_custom((L,A), num_labels, activation)
+            elif model_name == 'basset_custom':
+                model = model_zoo.basset_custom((L,A), num_labels, activation)
+            else:
+                print("can't find model")
+            print(model_name + '_' + activation)
 
-        # compile model model
-        auroc = tf.keras.metrics.AUC(curve='ROC', name='auroc')
-        aupr = tf.keras.metrics.AUC(curve='PR', name='aupr')
-        model.compile(tf.keras.optimizers.Adam(0.001), loss='binary_crossentropy', metrics=[auroc, aupr])
+            # compile model model
+            auroc = tf.keras.metrics.AUC(curve='ROC', name='auroc')
+            aupr = tf.keras.metrics.AUC(curve='PR', name='aupr')
+            model.compile(tf.keras.optimizers.Adam(0.001), loss='binary_crossentropy', metrics=[auroc, aupr])
 
-        # load model params
-        name = model_name + '_' + activation + '_' + str(trial)
-        model_dir = os.path.join(results_path, name+'_weights.h5')
-        model.load_weights(model_dir)
+            # load model params
+            name = model_name + '_' + activation + '_' + str(trial)
+            model_dir = os.path.join(results_path, name+'_weights.h5')
+            model.load_weights(model_dir)
 
-        # get test performance
-        test_results = model.evaluate(x_test, y_test)
-    
-        # save results
-        fout.write("%s\t%s\t%.4f\t%.4f\n"%(model_name, activation, test_results[1], test_results[2]))
+            # get test performance
+            test_results = model.evaluate(x_test, y_test)
+        
+            # save results
+            fout.write("%s\t%s\t%.4f\t%.4f\n"%(model_name, activation, test_results[1], test_results[2]))
 
 
 
